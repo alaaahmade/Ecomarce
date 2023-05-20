6bcc5a768ba92e844f86d9585c3ad660eb54c3ba
Alaa Ahmad <100975982+alaaahmade@users.noreply.github.com>
2023-05-20 20:24:53 +0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -5,7 +5,7 @@ let products =[
         price : 500,
         category : "phone",
         img : "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-card-40-iphone14pro-202209_FMT_WHH?wid=508&hei=472&fmt=p-jpg&qlt=95&.v=1663611329204",
-        disc : `This Is ** <br>This Is ** <br>This Is ** <br>`
+        disc : `This Is ** This Is ** This Is ** `
     },
     {
         id : Date.now(),
@@ -13,79 +13,69 @@ let products =[
         price : 500,
         category : "phone",
         img : "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-card-40-iphone14pro-202209_FMT_WHH?wid=508&hei=472&fmt=p-jpg&qlt=95&.v=1663611329204",
-        disc : `This Is ** <br>This Is ** <br>This Is ** <br>`
-    },
+disc : `This Is ** This Is ** This Is ** `    },
     {
         id : Date.now(),
         name : "IPhone",
         price : 500,
         category : "phone",
         img : "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-card-40-iphone14pro-202209_FMT_WHH?wid=508&hei=472&fmt=p-jpg&qlt=95&.v=1663611329204",
-        disc : `This Is ** <br>This Is ** <br>This Is ** <br>`
-    },
+disc : `This Is ** This Is ** This Is ** `    },
     {
         id : Date.now(),
         name : "IPhone",
         price : 500,
         category : "phone",
         img : "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-card-40-iphone14pro-202209_FMT_WHH?wid=508&hei=472&fmt=p-jpg&qlt=95&.v=1663611329204",
-        disc : `This Is ** <br>This Is ** <br>This Is ** <br>`
-    },
+disc : `This Is ** This Is ** This Is ** `    },
     {
         id : Date.now(),
         name : "IPhone",
         price : 500,
         category : "phone",
         img : "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-card-40-iphone14pro-202209_FMT_WHH?wid=508&hei=472&fmt=p-jpg&qlt=95&.v=1663611329204",
-        disc : `This Is ** <br>This Is ** <br>This Is ** <br>`
-    },
+disc : `This Is ** This Is ** This Is ** `    },
     {
         id : Date.now(),
         name : "A50",
         price : 500,
         category : "phone",
         img : "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-card-40-iphone14pro-202209_FMT_WHH?wid=508&hei=472&fmt=p-jpg&qlt=95&.v=1663611329204",
-        disc : `This Is ** <br>This Is ** <br>This Is ** <br>`
-    },
+disc : `This Is ** This Is ** This Is ** `    },
     {
         id : Date.now(),
         name : "IPhone",
         price : 500,
         category : "phone",
         img : "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-card-40-iphone14pro-202209_FMT_WHH?wid=508&hei=472&fmt=p-jpg&qlt=95&.v=1663611329204",
-        disc : `This Is ** <br>This Is ** <br>This Is ** <br>`
-    },
+disc : `This Is ** This Is ** This Is ** `    },
     {
         id : Date.now(),
         name : "A50",
         price : 500,
         category : "phone",
         img : "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-card-40-iphone14pro-202209_FMT_WHH?wid=508&hei=472&fmt=p-jpg&qlt=95&.v=1663611329204",
-        disc : `This Is ** <br>This Is ** <br>This Is ** <br>`
-    },
+disc : `This Is ** This Is ** This Is ** `    },
     {
         id : Date.now(),
         name : "IPhone",
         price : 500,
         category : "phone",
         img : "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-card-40-iphone14pro-202209_FMT_WHH?wid=508&hei=472&fmt=p-jpg&qlt=95&.v=1663611329204",
-        disc : `This Is ** <br>This Is ** <br>This Is ** <br>`
-    },
+disc : `This Is ** This Is ** This Is ** `    },
     {
         id : Date.now(),
         name : "IPhone",
         price : 500,
         category : "phone",
         img : "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-card-40-iphone14pro-202209_FMT_WHH?wid=508&hei=472&fmt=p-jpg&qlt=95&.v=1663611329204",
-        disc : `This Is ** <br>This Is ** <br>This Is ** <br>`
-    },
+disc : `This Is ** This Is ** This Is ** `    },
     {
         id : Date.now(),
         name : "HP",
         price : 500,
         category : "TV",
         img :"https://images.samsung.com/is/image/samsung/ps-ru7300-ue55ru7300uxmi-frontblack-241062844?$650_519_PNG$",
-        disc : `This Is ** <br>This Is ** <br>This Is ** <br>`
     },
     {
         id : Date.now(),
@@ -93,8 +83,7 @@ let products =[
         price : 500,
         category : "TV",
         img :"https://images.samsung.com/is/image/samsung/ps-ru7300-ue55ru7300uxmi-frontblack-241062844?$650_519_PNG$",
-        disc : `This Is ** <br>This Is ** <br>This Is ** <br>`
-    },
+disc : `This Is ** This Is ** This Is ** `    },
     
     {
         id : Date.now(),
@@ -102,7 +91,7 @@ let products =[
         price : 500,
         category : "TV",
         img :"https://images.samsung.com/is/image/samsung/ps-ru7300-ue55ru7300uxmi-frontblack-241062844?$650_519_PNG$",
-        disc : `This Is ** <br>This Is ** <br>This Is ** <br>`
+    disc : `This Is ** This Is ** This Is ** `    },
     },
     
     {
@@ -111,7 +100,7 @@ let products =[
         price : 500,
         category : "TV",
         img :"https://images.samsung.com/is/image/samsung/ps-ru7300-ue55ru7300uxmi-frontblack-241062844?$650_519_PNG$",
-        disc : `This Is ** <br>This Is ** <br>This Is ** <br>`
+    disc : `This Is ** This Is ** This Is ** `    },
     },
     
     {
@@ -120,7 +109,23 @@ let products =[
         price : 500,
         category : "TV",
         img :"https://images.samsung.com/is/image/samsung/ps-ru7300-ue55ru7300uxmi-frontblack-241062844?$650_519_PNG$",
-        disc : `This Is ** <br>This Is ** <br>This Is ** <br>`
+disc : `This Is ** This Is ** This Is ** `    },    
+    
+    {
+        id : Date.now(),
+        name : "Samsung",
+        price : 500,
+        category : "TV",
+        img :"https://images.samsung.com/is/image/samsung/ps-ru7300-ue55ru7300uxmi-frontblack-241062844?$650_519_PNG$",
+disc : `This Is ** This Is ** This Is ** `    },    
+    
+    {
+        id : Date.now(),
+        name : "Samsung",
+        price : 500,
+        category : "TV",
+        img :"https://images.samsung.com/is/image/samsung/ps-ru7300-ue55ru7300uxmi-frontblack-241062844?$650_519_PNG$",
+disc : `This Is ** This Is ** This Is ** ` 
     },
     
     {
@@ -129,8 +134,7 @@ let products =[
         price : 500,
         category : "TV",
         img :"https://images.samsung.com/is/image/samsung/ps-ru7300-ue55ru7300uxmi-frontblack-241062844?$650_519_PNG$",
-        disc : `This Is ** <br>This Is ** <br>This Is ** <br>`
-    },
+disc : `This Is ** This Is ** This Is ** `     },
     
     {
         id : Date.now(),
@@ -138,8 +142,7 @@ let products =[
         price : 500,
         category : "TV",
         img :"https://images.samsung.com/is/image/samsung/ps-ru7300-ue55ru7300uxmi-frontblack-241062844?$650_519_PNG$",
-        disc : `This Is ** <br>This Is ** <br>This Is ** <br>`
-    },
+disc : `This Is ** This Is ** This Is ** `     },
     
     {
         id : Date.now(),
@@ -147,82 +150,56 @@ let products =[
         price : 500,
         category : "TV",
         img :"https://images.samsung.com/is/image/samsung/ps-ru7300-ue55ru7300uxmi-frontblack-241062844?$650_519_PNG$",
-        disc : `This Is ** <br>This Is ** <br>This Is ** <br>`
-    },
-    
-    {
-        id : Date.now(),
-        name : "Samsung",
-        price : 500,
-        category : "TV",
-        img :"https://images.samsung.com/is/image/samsung/ps-ru7300-ue55ru7300uxmi-frontblack-241062844?$650_519_PNG$",
-        disc : `This Is ** <br>This Is ** <br>This Is ** <br>`
-    },
-    
-    {
-        id : Date.now(),
-        name : "Samsung",
-        price : 500,
-        category : "TV",
-        img :"https://images.samsung.com/is/image/samsung/ps-ru7300-ue55ru7300uxmi-frontblack-241062844?$650_519_PNG$",
-        disc : `This Is ** <br>This Is ** <br>This Is ** <br>`
-    },
+disc : `This Is ** This Is ** This Is ** `     },
     {
         id : Date.now(),
         name : "headphone",
         price : 500,
         category : "headphone",
         img :"http://cdn.shopify.com/s/files/1/0573/2309/4216/products/Miami_RubyRed_001_1200x1200.png?v=1656925796",
-        disc : `This Is ** <br>This Is ** <br>This Is ** <br>`
-    },
+disc : `This Is ** This Is ** This Is ** `     },
     {
         id : Date.now(),
         name : "headphone",
         price : 500,
         category : "headphone",
         img :"http://cdn.shopify.com/s/files/1/0573/2309/4216/products/Miami_RubyRed_001_1200x1200.png?v=1656925796",
-        disc : `This Is ** <br>This Is ** <br>This Is ** <br>`
-    },
+disc : `This Is ** This Is ** This Is ** `     },
     {
         id : Date.now(),
         name : "headphone",
         price : 500,
         category : "headphone",
         img :"http://cdn.shopify.com/s/files/1/0573/2309/4216/products/Miami_RubyRed_001_1200x1200.png?v=1656925796",
-        disc : `This Is ** <br>This Is ** <br>This Is ** <br>`
-    },
+disc : `This Is ** This Is ** This Is ** `     },
     {
         id : Date.now(),
         name : "headphone",
         price : 500,
         category : "headphone",
         img :"http://cdn.shopify.com/s/files/1/0573/2309/4216/products/Miami_RubyRed_001_1200x1200.png?v=1656925796",
-        disc : `This Is ** <br>This Is ** <br>This Is ** <br>`
-    },
+disc : `This Is ** This Is ** This Is ** `     },
     {
         id : Date.now(),
         name : "F500",
         price : 500,
         category : "headphone",
         img :"http://cdn.shopify.com/s/files/1/0573/2309/4216/products/Miami_RubyRed_001_1200x1200.png?v=1656925796",
-        disc : `This Is ** <br>This Is ** <br>This Is ** <br>`
-    },
+disc : `This Is ** This Is ** This Is ** `     },
     {
         id : Date.now(),
         name : "headphone",
         price : 500,
         category : "headphone",
         img :"http://cdn.shopify.com/s/files/1/0573/2309/4216/products/Miami_RubyRed_001_1200x1200.png?v=1656925796",
-        disc : `This Is ** <br>This Is ** <br>This Is ** <br>`
-    },
+disc : `This Is ** This Is ** This Is ** `     },
     {
         id : Date.now(),
         name : "F500",
         price : 500,
         category : "headphone",
         img :"http://cdn.shopify.com/s/files/1/0573/2309/4216/products/Miami_RubyRed_001_1200x1200.png?v=1656925796",
-        disc : `This Is ** <br>This Is ** <br>This Is ** <br>`
-
+disc : `This Is ** This Is ** This Is ** ` 
         
     },
     {
@@ -231,8 +208,7 @@ let products =[
         price : 500,
         category : "headphone",
         img :"http://cdn.shopify.com/s/files/1/0573/2309/4216/products/Miami_RubyRed_001_1200x1200.png?v=1656925796",
-        disc : `This Is ** <br>This Is ** <br>This Is ** <br>`
-
+disc : `This Is ** This Is ** This Is ** ` 
     },
     {
         id : Date.now(),
@@ -240,8 +216,7 @@ let products =[
         price : 500,
         category : "headphone",
         img :"http://cdn.shopify.com/s/files/1/0573/2309/4216/products/Miami_RubyRed_001_1200x1200.png?v=1656925796",
-        disc : `This Is ** <br>This Is ** <br>This Is ** <br>`
-
+disc : `This Is ** This Is ** This Is ** ` 
     },
     {
         id : Date.now(),
@@ -249,8 +224,7 @@ let products =[
         price : 500,
         category : "headphone",
         img :"http://cdn.shopify.com/s/files/1/0573/2309/4216/products/Miami_RubyRed_001_1200x1200.png?v=1656925796",
-        disc : `This Is ** <br>This Is ** <br>This Is ** <br>`
-
+disc : `This Is ** This Is ** This Is ** ` 
     },
     
     
